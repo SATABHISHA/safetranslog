98e3a24880d130346b80dc00728daf4b11f1b500
ArbSoftware <satabhishar@arbsoft.comm>
2023-04-28 16:34:48 +0530

    modified

--- SafeTransLogFlutterApp/lib/screens/login/verify_user_mobile.dart
+++ SafeTransLogFlutterApp/lib/screens/login/verify_user_mobile.dart
@@ -13,6 +13,64 @@ class VerifyUserMobile extends StatefulWidget {
 class _VerifyUserMobileState extends State<VerifyUserMobile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        // backgroundColor: Color.fromRGBO(2, 72, 254, 1.0),
+
+        /*appBar: AppBar(
+          // backgroundColor: Color.fromRGBO(71, 71, 71, 1.0),
+          backgroundColor: Color.fromRGBO(2, 72, 254, 1.0),
+          centerTitle: false,
+          automaticallyImplyLeading: false,
+          actions: [
+            Builder(
+                builder: (context) {
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Text('Hello ${EmployeeLogin.employeeJsonData['employee']['employee_full_name']} (${EmployeeLogin.employeeJsonData['user']['user_name']})', style: TextStyle(fontSize: 14, color: Colors.white),),
+                        // MediaQuery.of(context).size.width > 950 ? Text('Hello nnnd nfnnfnnf fnnfnfnfnf', style: TextStyle(fontSize: 14, color: Colors.white),) : SizedBox(),
+
+                        SizedBox(width: 10,),
+
+                      ],
+                    ),
+                  );
+                }
+            ),
+          ],
+
+        ),*/
+        body: Container(
+          // color: Colors.lightGreenAccent,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  // Color.fromRGBO(2, 72, 254, 1.0),
+                  Colors.blue,
+                  Colors.yellow,
+                  Colors.redAccent,
+
+                ],
+              )
+          ),
+          child: SafeArea(
+            child: Stack(
+                children: [
+
+                ]
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
