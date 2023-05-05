ca4a881f5f02e07b862c5a963832ef3c6a293cbd
ArbSoftware <satabhishar@arbsoft.comm>
2023-05-05 16:54:53 +0530

    modified

---
+++ SafeTransLogFlutterApp/lib/screens/bid/company_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompanyDetails extends StatefulWidget {
+  const CompanyDetails({Key? key}) : super(key: key);
+
+  @override
+  State<CompanyDetails> createState() => _CompanyDetailsState();
+}
+
+class _CompanyDetailsState extends State<CompanyDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
