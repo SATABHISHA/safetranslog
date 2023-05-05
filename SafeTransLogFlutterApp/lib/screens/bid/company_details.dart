321b80198821fde732c4817f74a32eb37701795e
ArbSoftware <satabhishar@arbsoft.comm>
2023-05-05 17:57:56 +0530

    modified

--- SafeTransLogFlutterApp/lib/screens/bid/company_details.dart
+++ SafeTransLogFlutterApp/lib/screens/bid/company_details.dart
@@ -10,6 +10,28 @@ class CompanyDetails extends StatefulWidget {
 class _CompanyDetailsState extends State<CompanyDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return WillPopScope(
+        onWillPop: () async {
+          return false;
+        },
+       child: Scaffold(
+        body: Container(
+          // color: Colors.lightGreenAccent,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  // Color.fromRGBO(2, 72, 254, 1.0),
+                  Colors.blue,
+                  Colors.yellow,
+                  Colors.redAccent,
+
+                ],
+              )
+          ),
+        ),
+    ),
+        );
   }
 }
