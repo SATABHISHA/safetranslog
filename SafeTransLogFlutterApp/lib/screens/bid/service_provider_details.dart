4ed4a5eb5125229fe7e8e68f1cd82a7e8c6c0a43
ArbSoftware <satabhishar@arbsoft.comm>
2023-05-10 15:47:19 +0530

    modified

--- SafeTransLogFlutterApp/lib/screens/bid/service_provider_details.dart
+++ SafeTransLogFlutterApp/lib/screens/bid/service_provider_details.dart
@@ -1,22 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
+import '../login/rounded_button.dart';
+
+enum Service {load, individual, transporter, agent}
 
 class ServiceProviderDetails extends StatefulWidget {
   // const ServiceProviderDetails({Key? key}) : super(key: key);
  static String id = 'ServiceProviderDetails';
-
   @override
   State<ServiceProviderDetails> createState() => _ServiceProviderDetailsState();
 }
 
 class _ServiceProviderDetailsState extends State<ServiceProviderDetails> {
+  Service? _character = Service.load;
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
         return false;
        },
-        child: Scaffold(),
+        child: Scaffold(
+          body: Container(
+            // color: Colors.lightGreenAccent,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    // Color.fromRGBO(2, 72, 254, 1.0),
+                    Colors.blue,
+                    Colors.yellow,
+                    Colors.redAccent,
+
+                  ],
+                )
+            ),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.max ,
+                children: [
+                  Center(child: Text('Service Provider', style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),)),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      child: Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(215, 227, 105, 1.0),
+                              blurRadius: 15.0,
+                              spreadRadius: 2.0,
+                              offset: Offset(0.0, 0.0),
+                            )
+                          ],
+                          // color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            colors: [
+                              Colors.white,
+                              Colors.lightGreenAccent,
+                            ],
+                          ),
+                        ),
+                        child: Stack(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0, 0, 0, 60),
+                              child: Column(
+                                children: [
+                                  SizedBox(height: 20,),
+                                  AutoSizeText(
+                                    'Few Information Before\nRegistration',
+                                    style: TextStyle(color: Color.fromRGBO(
+                                        37, 37, 37, 1.0),
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                    wrapWords: true,
+                                    minFontSize: 8,
+                                    maxFontSize: 20,
+                                    maxLines: 2,
+                                    textAlign: TextAlign.center,),
+                                  Expanded(
+                                    child: ListView(
+                                      shrinkWrap: true,
+                                      children: [
+                                        ListTile(
+                                          title: const Text('Load'),
+                                          leading: Radio<Service>(
+                                            value: Service.load,
+                                            groupValue: _character,
+                                            onChanged: (Service? value) {
+                                              setState(() {
+                                                _character = value;
+                                              });
+                                            },
+                                          ),
+                                          trailing: ,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            //----button code starts
+                            Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Container(
+                                width: double.infinity,
+                                height: 85,
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                  child: RoundedButton(colour: Color.fromRGBO(223, 137, 0, 1.0), title: 'NEXT', onPressed:() async {
+                                    // EasyLoading.show(status: 'Loading...');
+                                    // Navigator.pushNamed(context, ServiceProviderDetails.id);
+                                  }
+                                  ),
+                                ),
+                              ),
+                            ),
+                            //----button code ends
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
         );
   }
 }
