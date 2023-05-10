9c6d9d9f7ebd5182748bf79483993912e54a59b7
ArbSoftware <satabhishar@arbsoft.comm>
2023-05-10 13:11:43 +0530

    modified

--- SafeTransLogFlutterApp/lib/screens/bid/service_provider_details.dart
+++ SafeTransLogFlutterApp/lib/screens/bid/service_provider_details.dart
@@ -12,6 +12,11 @@ class ServiceProviderDetails extends StatefulWidget {
 class _ServiceProviderDetailsState extends State<ServiceProviderDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+       },
+        child: Scaffold(),
+        );
   }
 }
