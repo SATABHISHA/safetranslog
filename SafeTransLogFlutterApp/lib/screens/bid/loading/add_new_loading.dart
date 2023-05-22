28b20293eb9e54ef7fafa94f658be483a22ac1cb
ArbSoftware <satabhishar@arbsoft.comm>
2023-05-22 17:50:08 +0530

    modified

---
+++ SafeTransLogFlutterApp/lib/screens/bid/loading/add_new_loading.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddNewLoading extends StatefulWidget {
+  const AddNewLoading({Key? key}) : super(key: key);
+
+  @override
+  State<AddNewLoading> createState() => _AddNewLoadingState();
+}
+
+class _AddNewLoadingState extends State<AddNewLoading> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
