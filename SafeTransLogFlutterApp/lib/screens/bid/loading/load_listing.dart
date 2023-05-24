a65bfe8afea4bdc1f64e02ab7f0eee288244f4f5
ArbSoftware <satabhishar@arbsoft.comm>
2023-05-24 16:35:36 +0530

    modified

--- SafeTransLogFlutterApp/lib/screens/bid/loading/load_listing.dart
+++ SafeTransLogFlutterApp/lib/screens/bid/loading/load_listing.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:safetranslog/screens/login/rounded_button.dart';
 
 class LoadListing extends StatefulWidget {
   // const LoadListing({Key? key}) : super(key: key);
@@ -11,6 +13,91 @@ class LoadListing extends StatefulWidget {
 class _LoadListingState extends State<LoadListing> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        body: Container(
+          // color: Colors.lightGreenAccent,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  // Color.fromRGBO(2, 72, 254, 1.0),
+                  Colors.blue,
+                  Colors.yellow,
+                  Colors.redAccent,
+
+                ],
+              )
+          ),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.max ,
+              children: [
+                Center(child: Text('Load Listing', style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),)),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(215, 227, 105, 1.0),
+                            blurRadius: 15.0,
+                            spreadRadius: 2.0,
+                            offset: Offset(0.0, 0.0),
+                          )
+                        ],
+                        // color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.white,
+                            Colors.lightGreenAccent,
+                          ],
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+
+                          //----button code starts
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              width: double.infinity,
+                              height: 85,
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                child: RoundedButton(colour: Color.fromRGBO(223, 137, 0, 1.0), title: 'POST LOAD', onPressed:() async {
+                                  // EasyLoading.show(status: 'Loading...');
+                                  // Navigator.pushNamed(context, AddNewLoading.id);
+                                }
+                                ),
+                              ),
+                            ),
+                          ),
+                          //----button code ends
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- SafeTransLogFlutterApp/lib/screens/bid/loading/payment/payment_details.dart
+++ SafeTransLogFlutterApp/lib/screens/bid/loading/payment/payment_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:safetranslog/screens/bid/loading/load_listing.dart';
 import 'package:safetranslog/screens/login/rounded_button.dart';
 import 'package:safetranslog/screens/login/verify_user_mobile.dart';
 import 'package:safetranslog/widgets/reusable_common_widgets/constants.dart';
@@ -281,7 +282,7 @@ class _AddPaymentDetailsState extends State<AddPaymentDetails> {
                                 padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
                                 child: RoundedButton(colour: Color.fromRGBO(223, 137, 0, 1.0), title: 'NEXT', onPressed:() async {
                                   // EasyLoading.show(status: 'Loading...');
-                                  // Navigator.pushNamed(context, AddNewLoading.id);
+                                  Navigator.pushNamed(context, LoadListing.id);
                                 }
                                 ),
                               ),
