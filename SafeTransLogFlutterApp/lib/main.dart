7a0c47f802cec66787473bab272d51b68ecb2dab
Satabhisha Roy <satabhisha.innits@gmail.com>
2023-05-28 09:30:15 +0530

    weekend work

--- SafeTransLogFlutterApp/lib/main.dart
+++ SafeTransLogFlutterApp/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:safetranslog/screens/bid/company_details.dart';
 import 'package:safetranslog/screens/bid/loading/add_loading.dart';
+import 'package:safetranslog/screens/bid/loading/bid_confirmation.dart';
 import 'package:safetranslog/screens/bid/loading/load_listing.dart';
 import 'package:safetranslog/screens/bid/loading/payment/payment_details.dart';
+import 'package:safetranslog/screens/bid/place_bid.dart';
 import 'package:safetranslog/screens/bid/service_provider_details.dart';
 import 'package:safetranslog/screens/login/login_user.dart';
 import 'package:safetranslog/screens/login/verify_user_mobile.dart';
@@ -38,6 +40,8 @@ class MyApp extends StatelessWidget {
         AddNewLoading.id: (context)=> AddNewLoading(),
         AddPaymentDetails.id: (context)=> AddPaymentDetails(),
         LoadListing.id: (context)=> LoadListing(),
+        PlaceBid.id: (context)=> PlaceBid(),
+        BidConfirmation.id: (context)=> BidConfirmation(),
       },
     );
   }
--- SafeTransLogFlutterApp/lib/screens/bid/loading/load_listing.dart
+++ SafeTransLogFlutterApp/lib/screens/bid/loading/load_listing.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:safetranslog/screens/bid/place_bid.dart';
 import 'package:safetranslog/screens/login/rounded_button.dart';
 
 class LoadListing extends StatefulWidget {
@@ -75,82 +76,85 @@ class _LoadListingState extends State<LoadListing> {
                               shrinkWrap: true,
                               itemCount: 1,
                               itemBuilder: (context, int index) {
-                                return Container(
-                                  width: double.infinity,
-                                  // height: 200,
-                                  decoration: BoxDecoration(color: Color.fromRGBO(242, 241, 248, 1.0), borderRadius: BorderRadius.circular(10.0)),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Padding(
-                                        padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                                        child: Row(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            AutoSizeText('Kolkata To Shiliguri', style: TextStyle(color: Colors.blue, fontSize: 14),)
-                                          ],
+                                return Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    width: double.infinity,
+                                    height: 200,
+                                    decoration: BoxDecoration(color: Color.fromRGBO(242, 241, 248, 1.0), borderRadius: BorderRadius.circular(10.0)),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                                          child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              AutoSizeText('Kolkata To Shiliguri', style: TextStyle(color: Colors.blue, fontSize: 14),)
+                                            ],
+                                          ),
                                         ),
-                                      ),
-                                      SizedBox(height: 10,),
-                                      Expanded(
-                                        child: Row(
-                                          children: [
-                                            Flexible(
-                                              flex: 1,
-                                              child: Padding(
-                                                padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                                child: Column(
-                                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                                  children: [
-                                                    AutoSizeText('LCV Required', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                                                    SizedBox(height: 5,),
-                                                    Row(children: [Image.asset('images/truckpic.png', width: 25, height: 17.5,), SizedBox(width: 10,), AutoSizeText('4 Tones LCV', style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),) ],),
-                                                    SizedBox(height: 15,),
-                                                    AutoSizeText('Rs 24000.00', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.redAccent)),
-                                                    AutoSizeText('(asked price)', style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal, color: Colors.black)),
-                                                  ],
+                                        SizedBox(height: 10,),
+                                        Expanded(
+                                          child: Row(
+                                            children: [
+                                              Flexible(
+                                                flex: 1,
+                                                child: Padding(
+                                                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                                                  child: Column(
+                                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                                    children: [
+                                                      AutoSizeText('LCV Required', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                                                      SizedBox(height: 5,),
+                                                      Row(children: [Image.asset('images/truckpic.png', width: 25, height: 17.5,), SizedBox(width: 10,), AutoSizeText('4 Tones LCV', style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),) ],),
+                                                      SizedBox(height: 15,),
+                                                      AutoSizeText('Rs 24000.00', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.redAccent)),
+                                                      AutoSizeText('(asked price)', style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal, color: Colors.black)),
+                                                    ],
+                                                  ),
                                                 ),
                                               ),
-                                            ),
-                                            Flexible(
-                                              flex: 1,
-                                              child: Padding(
-                                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                                child: Column(
-                                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                                  children: [
-                                                    Center(
-                                                      child: Container(width: 54, height: 54, decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Color.fromRGBO(187, 186, 180, 1.0)), child: Padding(
-                                                        padding: const EdgeInsets.all(5.0),
-                                                        child: Image.asset('images/profile.png'),
-                                                      ),),
-                                                    ),
-                                                    SizedBox(height: 10,),
-                                                    AutoSizeText('Calcutta Express\nCompany', style: TextStyle(fontSize: 14, color: Colors.black),textAlign: TextAlign.center,),
-                                                    SizedBox(height: 10,),
-                                                    //----button code starts
-                                                    Expanded(
-                                                      child: Container(
-                                                        width: double.infinity,
-                                                        child: Padding(
-                                                          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                                                          child: RoundedButton(colour: Color.fromRGBO(244, 80, 49, 1.0), title: 'Place Bid', onPressed:() async {
-                                                            // EasyLoading.show(status: 'Loading...');
-                                                            // Navigator.pushNamed(context, AddNewLoading.id);
-                                                          }
+                                              Flexible(
+                                                flex: 1,
+                                                child: Padding(
+                                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                                  child: Column(
+                                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                                    children: [
+                                                      Center(
+                                                        child: Container(width: 54, height: 54, decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: Color.fromRGBO(187, 186, 180, 1.0)), child: Padding(
+                                                          padding: const EdgeInsets.all(5.0),
+                                                          child: Image.asset('images/profile.png'),
+                                                        ),),
+                                                      ),
+                                                      SizedBox(height: 10,),
+                                                      AutoSizeText('Calcutta Express\nCompany', style: TextStyle(fontSize: 14, color: Colors.black),textAlign: TextAlign.center,),
+                                                      SizedBox(height: 10,),
+                                                      //----button code starts
+                                                      Expanded(
+                                                        child: Container(
+                                                          width: double.infinity,
+                                                          child: Padding(
+                                                            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                                            child: RoundedButton(colour: Color.fromRGBO(244, 80, 49, 1.0), title: 'Place Bid', onPressed:() async {
+                                                              // EasyLoading.show(status: 'Loading...');
+                                                              // Navigator.pushNamed(context, AddNewLoading.id);
+                                                            }
+                                                            ),
                                                           ),
                                                         ),
                                                       ),
-                                                    ),
-                                                    //----button code ends
-                                                  ],
+                                                      //----button code ends
+                                                    ],
+                                                  ),
                                                 ),
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 );
                               }
@@ -165,7 +169,7 @@ class _LoadListingState extends State<LoadListing> {
                                 padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
                                 child: RoundedButton(colour: Color.fromRGBO(223, 137, 0, 1.0), title: 'POST LOAD', onPressed:() async {
                                   // EasyLoading.show(status: 'Loading...');
-                                  // Navigator.pushNamed(context, AddNewLoading.id);
+                                  Navigator.pushNamed(context, PlaceBid.id);
                                 }
                                 ),
                               ),
